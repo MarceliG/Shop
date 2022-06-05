7b98854707f23385e66d660032976e3fdffcbe9c
MarceliG <marcel9817@gmail.com>
2022-06-05 17:02:33 +0200

    9#Cookie_method_csrftoken

--- static/js/cart.js
+++ static/js/cart.js
@@ -2,15 +2,39 @@ var updateBtns = document.getElementsByClassName('update-cart')
 console.log('hello')
 for (var i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function () {
-        var productId = this.dataset.product
+        var product_id = this.dataset.product
         var action = this.dataset.action
-        console.log('productId:', productId, 'action:', action)
+        console.log('product_id:', product_id, 'action:', action)
 
         console.log('USER:', user)
         if (user === 'AnonymusUser') {
             console.log('Not logged in')
         } else {
-            console.log('User is logged in, sending data...')
+            update_user_order(product_id, action)
         }
     })
 }
+
+function update_user_order(product_id, action) {
+    console.log('User is logged in, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({ 'product_id': product_id, 'action': action })
+    })
+
+        .then((response) => {
+            return response.json()
+        })
+
+        .then((data) => {
+            console.log('data:', data)
+        })
+}
+
