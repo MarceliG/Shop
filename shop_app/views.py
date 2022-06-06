5ba252cef76a62fa4754da68c74ff2787c0aba9a
MarceliG <marcel9817@gmail.com>
2022-06-06 14:10:04 +0200

    adding_product_to_cart

--- shop_app/views.py
+++ shop_app/views.py
@@ -70,4 +70,24 @@ def update_item(request):
     action = data["action"]
     print("Action: ", action)
     print("Product_id: ", product_id)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=product_id)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False
+    )
+    order_item, created = OrderItem.objects.get_or_create(
+        order=order, product=product
+    )
+
+    if action == "add":
+        order_item.quantity = order_item.quantity + 1
+    elif action == "remove":
+        order_item.quantity = order_item.quantity - 1
+
+    order_item.save()
+
+    if order_item.quantity <= 0:
+        order_item.delete()
+
     return JsonResponse("Item was added", safe=False)
